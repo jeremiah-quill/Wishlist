cf2dcac1e7da3c8ed06bc6a4b4d810a1dff165ae
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-12-04 09:07:22 -0500

    configured sequalize in connection.js and completed most of necessary connections/installs in server.js, added proprosal outline

---
+++ proposal.md
@@ -0,0 +1,27 @@
+## Title
+Gift Exchange Manager
+
+## Description (1 paragraph)
+An application where a user can 
+
+## APIs (Third-party)
+
+
+## Wireframe or Design for the website
+![Alt text]()
+
+## List of Tasks or Acceptance Criteria
+
+```
+
+
+```
+
+## First Task for each member
+Jeremiah:
+
+Sam: 
+
+Phalen: 
+
+## What does the functioning prototype have in features for the first week?
---
+++ server.js
@@ -0,0 +1,27 @@
+// install dependencies
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(require('./controllers/'));
+const sequelize = require('./config/connection');
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Sets up the Express app to handle data parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
