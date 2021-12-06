fba7755d80eb6bec970238485f9a2fc2ddb2aee0
JQ <jcq5010@gmail.com>
2021-12-05 22:02:44 -0500

    added update routes for group details and adding group member

--- controllers/api/groupRoutes.js
+++ controllers/api/groupRoutes.js
@@ -4,7 +4,7 @@ const { Group, User, UserGroup } = require("../../models");
 
 // Use for testing what groups have been created
 groupRoutes.get("/", async (req, res) => {
-  const groupData = await Group.findAll();
+  const groupData = await Group.findAll({ include: [{ model: User }] });
   res.json(groupData);
 });
 
@@ -18,7 +18,7 @@ groupRoutes.get("/:id", async (req, res) => {
       res.json("Group does not exist");
       return;
     }
-    // TODO: change below to res.render
+    // TODO: change below to res.render so we can pass groupData to a handlebars view
     res.status(200).json(groupData);
   } catch (err) {
     res.status(500).json(`Could not find a group with that id`);
@@ -27,7 +27,13 @@ groupRoutes.get("/:id", async (req, res) => {
 
 // Create a new group.  Pass in the creating user as user_id, and they will be added as the first group member
 groupRoutes.post("/", (req, res) => {
-  console.log(req.body);
+  // req.body will look like:
+  // {
+  //   "event_name": "event_name",
+  //   "price_limit": 10,
+  //   "event_date": "1/1/22",
+  //   "user_id": 1
+  // }
   Group.create({
     event_name: req.body.event_name,
     price_limit: req.body.price_limit,
@@ -38,11 +44,42 @@ groupRoutes.post("/", (req, res) => {
       user_id: req.body.user_id,
     })
       .then((usergroup) => {
+        // TODO: add a redirect
         res.status(200).json(usergroup);
       })
       .catch((err) => {
         res.status(500).json(err);
       });
+  });
+});
+
+// add a user to a group
+groupRoutes.put("/:id/add", (req, res) => {
+  UserGroup.create({
+    user_id: req.body.user_id,
+    group_id: req.params.id,
+  }).then((group) => {
+    res.json(group);
+  });
+});
+
+// update group details
+groupRoutes.put("/:id", (req, res) => {
+  // update group details based on req.body
+  Group.update(
+    {
+      event_name: req.body.event_name,
+      price_limit: req.body.price_limit,
+      event_date: req.body.event_date,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  ).then(() => {
+    // if there is a user_id passed into req.body, then create a new relationship between the user_id and the group_id
+    res.json("testing");
   });
 });
 
